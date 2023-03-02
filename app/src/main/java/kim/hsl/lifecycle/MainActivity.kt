cd3ffa86532c3149576e3b209f8092d91c34874b
han1202012 <octopus_truth@163.com>
2023-03-03 00:26:34 +0800

    update

--- app/src/main/java/kim/hsl/lifecycle/MainActivity.kt
+++ app/src/main/java/kim/hsl/lifecycle/MainActivity.kt
@@ -2,10 +2,29 @@ package kim.hsl.lifecycle
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Chronometer
 
 class MainActivity : AppCompatActivity() {
+
+    /**
+     * UI 组件的业务逻辑 与 Activity 系统组件 绑定程度很高
+     * 并且 UI 组件的逻辑 与 Activity 生命周期关联程度很高
+     */
+    lateinit var chronometer: Chronometer
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        chronometer = findViewById(R.id.chronometer)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        chronometer.start()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        chronometer.stop()
     }
 }
